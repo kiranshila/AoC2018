a5e3a1a82b324d4380a18e25e9b4d0681c08fca8
Kiran Shila <kiranshila@mail.usf.edu>
2018-12-10 00:40:41 -0500

    Day 9

--- AoC_9.jl
+++ AoC_9.jl
@@ -3,24 +3,31 @@
     Day 9
     Kiran Shila
 ==#
-numPlayers = 9
-lastPoints = 46
+numPlayers = 479
+lastPoints = 71035*100
 
+#==
 mutable struct Marble
     isCurrent::Bool
     value::Int64
-    CW
-    CCW
+    CW::Marble
+    CCW::Marble
     # Inner Constructors
     Marble() = (x = new();x.isCurrent = true;x.value = 0; x.CW = x; x.CCW = x)
     Marble(isCurrrent,value,CW,CCW) = new(isCurrrent,value,CW,CCW)
 end
+==#
+
 
 function Base.string(x::Marble)
-    if x.CW.value == 0
+    if x.CW.value == 0 && x.isCurrent
+        return "($(x.value))"
+    elseif x.CW.value == 0 && !x.isCurrent
         return "$(x.value)"
+    elseif x.CW.value != 0 && x.isCurrent
+        return "($(x.value)) " * string(x.CW)
     else
-        return "$(x.value) => " * string(x.CW)
+        return "$(x.value) " * string(x.CW)
     end
 end
 
@@ -29,6 +36,7 @@ Base.display(x::Marble) = print(x)
 
 # Create 0 Marble
 currentMarble = Marble()
+zeroMarble = currentMarble
 
 function insertMarbleAtCurrent!(number::Int64,thisMarble::Marble)
     thisMarble.isCurrent = false
@@ -43,14 +51,14 @@ function removeMarble!(thisMarble::Marble)
     thisMarble.CW.CCW = thisMarble.CCW
 end
 
-players = zeros(Int64,numPlayers)
+scores = zeros(Int64,numPlayers)
 
-function playGame(players::Vector{Int64},marbleNum::Int64,currentMarble::Marble,player::Int64)
+function playGame(scores::Vector{Int64},marbleNum::Int64,currentMarble::Marble,player::Int64)
     if mod(marbleNum,23) != 0
         return insertMarbleAtCurrent!(marbleNum,currentMarble)
     else
         sevenAway = currentMarble.CCW.CCW.CCW.CCW.CCW.CCW.CCW
-        players[player] = marbleNum + sevenAway.value
+        scores[player] += marbleNum + sevenAway.value
         currentMarble.isCurrent = false
         sevenAway.CW.isCurrent = true
         removeMarble!(sevenAway)
@@ -58,8 +66,8 @@ function playGame(players::Vector{Int64},marbleNum::Int64,currentMarble::Marble,
     end
 end
 
-Juno.@progress for marble = 1:lastPoints
-    global currentMarble = playGame(players,marble,currentMarble,mod(marble-1,numPlayers)+1)
+@time Juno.@progress for marble = 1:lastPoints
+    global currentMarble = playGame(scores,marble,currentMarble,mod(marble-1,numPlayers)+1)
 end
 
-print(maximum(players))
+print(maximum(scores))
